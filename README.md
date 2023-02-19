9d55a129562cfda8c45a845509e59638642c1508
김문기 <mg.kim@autowini.com>
2023-02-19 13:03:13 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPMTestProject
+
+A description of this package.
---
+++ Sources/SPMTestProject/SPMTestProject.swift
@@ -0,0 +1,6 @@
+public struct SPMTestProject {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
